19c78f815b4750800379d69923609f2422180720
Nazaniinkarimian@gmail.com <nazaniinkarimian@gmail.com>
2026-01-02 19:50:37 +0330

    Review of the if template tag in Django

--- challenges/views.py
+++ challenges/views.py
@@ -14,19 +14,23 @@ days ={
     'tuesday': 'this is tuesday in dictionary',
     'wednesday': 'this is wednesday in dictionary',
     'thursday': 'this is thursday in dictionary',
-    'friday': 'this is friday in dictionary',
+    'friday': None
 }
 
 
 def days_list(request):
     days_list = list(days.keys())
-    list_items = ""
+    # list_items = ""
     
-    for day in days_list:
-        url_path = reverse('days-of-week', args=[day])
-        list_items += f'<li> <a href="{url_path}"> {day} </a> </li>'
-    content = f'<ul> \n {list_items} \n</ul>'
-    return HttpResponse(content)
+    # for day in days_list:
+    #     url_path = reverse('days-of-week', args=[day])
+    #     list_items += f'<li> <a href="{url_path}"> {day} </a> </li>'
+    # content = f'<ul> \n {list_items} \n</ul>'
+    # return HttpResponse(content)
+    context = {
+        'days':days_list
+    }
+    return render(request, "challenges/index.html", context)
 
 def dynamic_days_by_number(request, day):
     days_names = list(days.keys())
@@ -43,13 +47,15 @@ def dynamic_days_by_number(request, day):
 
 def dynamic_days(request, day):
     day_data = days.get(day)
-    if day_data is not None:
+    # if day_data is not None:
+    #     day_data = "there is no data"
         context = {
-            'days': days_list
+            "data": day_data,
+            "day": f'selected DAY is {day}'
         }
        
         # DTL -> Django Template Language
-        return render(request, 'challenges/index.html', context)
+        return render(request, 'challenges/challenge.html', context)
         # response_data = render_to_string('challenges/challenges.html')
         # return HttpResponse(response_data)
     return HttpResponseNotFound('day does not exists')

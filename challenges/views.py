bf788cf4d27b73d4525d83ad4ba35a89c1883690
Nazaniinkarimian@gmail.com <nazaniinkarimian@gmail.com>
2026-01-02 21:07:15 +0330

    Review of the 404 Not Found page

--- challenges/views.py
+++ challenges/views.py
@@ -14,19 +14,19 @@ days ={
     'tuesday': 'this is tuesday in dictionary',
     'wednesday': 'this is wednesday in dictionary',
     'thursday': 'this is thursday in dictionary',
-    'friday': None
+    'friday': 'this is friday in dictionary',
 }
 
 
 def days_list(request):
     days_list = list(days.keys())
-    list_items = ""
+    # list_items = ""
     
-    for day in days_list:
-        url_path = reverse('days-of-week', args=[day])
-        list_items += f'<li> <a href="{url_path}"> {day} </a> </li>'
-    content = f'<ul> \n {list_items} \n</ul>'
-    return HttpResponse(content)
+    # for day in days_list:
+    #     url_path = reverse('days-of-week', args=[day])
+    #     list_items += f'<li> <a href="{url_path}"> {day} </a> </li>'
+    # content = f'<ul> \n {list_items} \n</ul>'
+    # return HttpResponse(content)
     context = {
         'days':days_list
     }
@@ -47,8 +47,9 @@ def dynamic_days_by_number(request, day):
 
 def dynamic_days(request, day):
     day_data = days.get(day)
-    # if day_data is not None:
-    #     day_data = "there is no data"
+    if day_data is None:
+        # response_data = render_to_string('404.html')
+        # return HttpResponseNotFound(response_data)
     context = {
             "data": day_data,
             "day": f'selected DAY is {day}',
@@ -56,6 +57,6 @@ def dynamic_days(request, day):
        
         # DTL -> Django Template Language
     return render(request, 'challenges/challenge.html', context)
-    response_data = render_to_string('challenges/challenges.html')
-    return HttpResponse(response_data)
-    return HttpResponseNotFound('day does not exists')
+    # response_data = render_to_string('challenges/challenges.html')
+    # return HttpResponse(response_data)
+    # return HttpResponseNotFound('day does not exists')
